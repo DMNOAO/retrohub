68f6f08043107d50f1c4a2ff954e599445a22a69
DMNOAO <167371629+DMNOAO@users.noreply.github.com>
2026-08-03 00:26:03 -0400

    Prueba ubicaciones iniciales de Esmeralda

--- test/features/pokemon/decoder/pokemon_decoder_gen3_test.dart
+++ test/features/pokemon/decoder/pokemon_decoder_gen3_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:retrohub/features/pokemon/decoder/pokemon_decoder.dart';
+import 'package:retrohub/features/pokemon/models/pokemon_game_profile.dart';
 
 void main() {
   group('PokemonDecoder Gen III', () {
@@ -17,6 +18,21 @@ void main() {
         PokemonDecoder.decodeGen3Text(<int>[0xBB, 0x01, 0xBC, 0xFF]),
         'AB',
       );
+    });
+
+    test('traduce los mapas iniciales de Esmeralda', () {
+      final PokemonGameProfile profile =
+          PokemonGameProfile.fromGameIdentity(
+        gameTitle: 'Pokémon Esmeralda',
+        romPath: 'pokemon_esmeralda.gba',
+      );
+
+      expect(PokemonDecoder.mapName(profile, 0x0009), 'Villa Raíz');
+      expect(
+        PokemonDecoder.mapName(profile, 0x0104),
+        'Laboratorio del Profesor Abedul',
+      );
+      expect(PokemonDecoder.mapName(profile, 0x0010), 'Ruta 101');
     });
   });
 }
