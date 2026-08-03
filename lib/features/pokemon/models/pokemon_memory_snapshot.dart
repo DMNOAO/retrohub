1cef981f75e27a36e5cf25a9cecf206ea29e9302
DMNOAO <167371629+DMNOAO@users.noreply.github.com>
2026-08-02 23:28:37 -0400

    Conserva el tiempo leído desde Gen III

--- lib/features/pokemon/models/pokemon_memory_snapshot.dart
+++ lib/features/pokemon/models/pokemon_memory_snapshot.dart
@@ -27,6 +27,7 @@ class PokemonMemorySnapshot {
   final List<int> seenPokemonIds;
   final List<int> caughtPokemonIds;
   final List<PokemonPartyMember> party;
+  final int? gamePlayTimeMinutes;
   // Combate (Fase 4.2/4.3). null = no soportado en esta versión/perfil.
   // battleState: 0 = fuera de combate, 1 = combate salvaje, 2 = combate
   // de entrenador (mismo significado en Gen1 y Gen2, direcciones
@@ -35,7 +36,7 @@ class PokemonMemorySnapshot {
   final int? otherTrainerClassId;
   final int? otherTrainerId;
   final int? battleResultRaw;
-  const PokemonMemorySnapshot({required this.capturedAt,required this.profile,required this.memoryShift,required this.playerName,required this.trainerId,required this.currentMapId,required this.playerX,required this.playerY,required this.money,required this.badgesMask,required this.pokedexSeen,required this.pokedexCaught,required this.seenPokemonIds,required this.caughtPokemonIds,required this.party,this.battleState,this.otherTrainerClassId,this.otherTrainerId,this.battleResultRaw});
+  const PokemonMemorySnapshot({required this.capturedAt,required this.profile,required this.memoryShift,required this.playerName,required this.trainerId,required this.currentMapId,required this.playerX,required this.playerY,required this.money,required this.badgesMask,required this.pokedexSeen,required this.pokedexCaught,required this.seenPokemonIds,required this.caughtPokemonIds,required this.party,this.gamePlayTimeMinutes,this.battleState,this.otherTrainerClassId,this.otherTrainerId,this.battleResultRaw});
   List<int> get partySpeciesIds=>party.map((e)=>e.pokedexId).toList(growable:false);
   int get badgeCount=>PokemonDecoder.countBits(<int>[badgesMask&0xff,(badgesMask>>8)&0xff]);
   String get currentLocation=>PokemonDecoder.mapName(profile,currentMapId);
