d748e96201095e5bfdbbbf4598b655650dc0c982
DMNOAO <167371629+DMNOAO@users.noreply.github.com>
2026-08-18 12:27:04 -0400

    revert: keep original sprite backgrounds

--- lib/core/assets/sprite_image.dart
+++ lib/core/assets/sprite_image.dart
@@ -7,7 +7,6 @@ class SpriteImage extends StatelessWidget {
   final double size;
   final IconData fallbackIcon;
   final BoxFit fit;
-  final bool removeWhiteBackground;
 
   const SpriteImage({
     super.key,
@@ -15,7 +14,6 @@ class SpriteImage extends StatelessWidget {
     this.size = 48,
     this.fallbackIcon = Icons.image_not_supported_outlined,
     this.fit = BoxFit.contain,
-    this.removeWhiteBackground = false,
   });
 
   @override
@@ -45,19 +43,7 @@ class SpriteImage extends StatelessWidget {
       );
     }
 
-    final rendered = removeWhiteBackground
-        ? ColorFiltered(
-            colorFilter: const ColorFilter.matrix(<double>[
-              1, 0, 0, 0, 0,
-              0, 1, 0, 0, 0,
-              0, 0, 1, 0, 0,
-              -1, -1, -1, 1, 510,
-            ]),
-            child: image,
-          )
-        : image;
-
-    return SizedBox(width: size, height: size, child: rendered);
+    return SizedBox(width: size, height: size, child: image);
   }
 
   Widget _fallback() {
