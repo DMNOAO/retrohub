2359173ecc6f38c06543b9411338d1e34f41acf9
DMNOAO <167371629+DMNOAO@users.noreply.github.com>
2026-08-18 12:18:23 -0400

    feat: support transparent white backgrounds for legacy sprites

--- lib/core/assets/sprite_image.dart
+++ lib/core/assets/sprite_image.dart
@@ -7,6 +7,7 @@ class SpriteImage extends StatelessWidget {
   final double size;
   final IconData fallbackIcon;
   final BoxFit fit;
+  final bool removeWhiteBackground;
 
   const SpriteImage({
     super.key,
@@ -14,6 +15,7 @@ class SpriteImage extends StatelessWidget {
     this.size = 48,
     this.fallbackIcon = Icons.image_not_supported_outlined,
     this.fit = BoxFit.contain,
+    this.removeWhiteBackground = false,
   });
 
   @override
@@ -43,7 +45,19 @@ class SpriteImage extends StatelessWidget {
       );
     }
 
-    return SizedBox(width: size, height: size, child: image);
+    final rendered = removeWhiteBackground
+        ? ColorFiltered(
+            colorFilter: const ColorFilter.matrix(<double>[
+              1, 0, 0, 0, 0,
+              0, 1, 0, 0, 0,
+              0, 0, 1, 0, 0,
+              -1, -1, -1, 1, 510,
+            ]),
+            child: image,
+          )
+        : image;
+
+    return SizedBox(width: size, height: size, child: rendered);
   }
 
   Widget _fallback() {
