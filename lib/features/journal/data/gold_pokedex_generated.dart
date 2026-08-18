46ffb69cda0cbcf2609af385472c7548c06d9275
DMNOAO <167371629+DMNOAO@users.noreply.github.com>
2026-08-18 15:32:48 -0400

    chore: point Gold dataset to shared Gen II generator

--- lib/features/journal/data/gold_pokedex_generated.dart
+++ lib/features/journal/data/gold_pokedex_generated.dart
@@ -1,4 +1,4 @@
 import 'pokedex_models.dart';
 
-// GENERATED FILE. Run: dart run tool/generate_gold_pokedex.dart
+// GENERATED FILE. Run: dart run tool/generate_gold_silver_pokedex.dart
 const Map<int, PokedexSpeciesDetail> goldGeneratedSpecies = {};
